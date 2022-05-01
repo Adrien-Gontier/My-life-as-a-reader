df1fbfb84f6be831c13231605b1cd8ce29f7b609
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-05-01 20:41:02 +0200

    Delete script.js
    Files deleted.

--- script.js
+++
@@ -1,9 +0,0 @@
-const message =
-  "Thank you for your message. I will answer soon.";
-
-document
-  .getElementById("contactform")
-  .addEventListener("submit", function(event) {
-    event.preventDefault();
-    alert(message);
-  });
