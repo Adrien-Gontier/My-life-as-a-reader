013e50a2c4671acfecd449fc4d45345520998c9d
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-05-24 17:23:16 +0200

    Update navbar.php
    Line 1-21 : code of the navbar added.

---
+++ navbar.php
@@ -0,0 +1,21 @@
+<div>  <!-- Title of the other pages of the website by link. It's in flex-box. -->
+    <a href="index.html">Home</a>  <!-- Link to Home page. -->
+</div>
+ <div>
+    <a href="reading_in_progress.html">Reading in progress</a>  <!-- Link to the page "Reading in progress". -->
+</div>
+<div>
+    <a href="reading_finished.html">Reading finished</a>  <!-- Link to the page "Reading finished". -->
+</div>
+<div>
+    <a href="pile_to_read.html">Pile to read</a>  <!-- Link to the page "Pile to read". -->
+</div>
+<div>
+    <a href="wishlist.html">Wishlist</a>  <!-- Link to the page "Wishlist". -->
+</div>
+<div>
+    <a href="contact.html">Contact</a>  <!-- Link to the page "Contact". -->
+</div>
+<div>
+    <a href="who_am_i.html">Who am I</a>  <!-- Link to the page "Who am I". -->
+</div>
