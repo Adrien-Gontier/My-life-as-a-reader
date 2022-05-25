9c4721245bc2c6c1c812d6fd8856430564fc3c12
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-05-25 10:43:37 +0200

    Create who_am_i.php
    File with .php instead of .html
    
    Line 14 : navbar replaced by a php file navbar.php
    
    Line 35 : footer replaced by a php file footer.php

---
+++ who_am_i.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">  <!-- The langage of this page is "english" -->
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Who am I - My life as a reader</title>  <!-- Thumb index of the page. -->
+    <link href="style.css" rel="stylesheet" type="text/css" />  <!-- This file is connect to the file "style.css" for the stylesheet. -->
+  </head>
+  <body>
+    <header>
+      <a href="index.html" class="website_title">My life as a reader</a>  <!-- Title of the website and link to index.html -->
+      <nav>
+
+        <?php include('navbar.php'); ?>
+            
+      </nav>
+     </header>
+     <div class="page_title_head">
+      <div class="page_title_area"> 
+        <a id="who_am_i" class="page_title">Who am I</a>  <!-- Title of the page. -->
+      </div>
+        <img class="picture_15_100_size" src="pictures/pexels-ricardo-esquivel-1907785.jpg" alt="Ninety degree shelves full of books"> 
+      </div>
+    <section class="story_of_my_life">  <!-- My biography. -->
+      <p>My name is Adrien Gontier. I read for years.</p> 
+      <p>I like detective novels, romances, dramas, thrillers, adventures, ...</p>
+      <p>I don't stick to any genre.</p>
+      <p>For writers, I read John Grisham, Dan Brown, Georges Simenon, Stieg Larsson, ...</p>
+      <p>When I read, I really like to listen to the soundtracks of the films and TV series from which the adaptations are taken. It puts me in a certain mood, a certain atmosphere that I like.</p>
+      <p>However, I have a code. I never spoil the plots of the works I read. Because I hate when people do that to me.</p>
+      <p>If I can give you any advice. Only one. For anyone who wants to start reading. It is to begin.</p>
+    </section>  
+    <footer>
+
+    <?php include('footer.php'); ?>
+    
+    </footer>
+  </body>
+</html>
