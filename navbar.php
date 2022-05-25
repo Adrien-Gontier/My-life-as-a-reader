6cbc6d4983d4cbfd5fefa3338999dedb348fb163
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-05-25 10:45:36 +0200

    Update navbar.php
    Line 5, 8, 11, 14, 17 and 20 : links of the navbar added with .php for each link added instead of .html

--- navbar.php
+++ navbar.php
@@ -2,20 +2,20 @@
     <a href="index.html">Home</a>  <!-- Link to Home page. -->
 </div>
  <div>
-    <a href="reading_in_progress.html">Reading in progress</a>  <!-- Link to the page "Reading in progress". -->
+    <a href="reading_in_progress.php">Reading in progress</a>  <!-- Link to the page "Reading in progress". -->
 </div>
 <div>
-    <a href="reading_finished.html">Reading finished</a>  <!-- Link to the page "Reading finished". -->
+    <a href="reading_finished.php">Reading finished</a>  <!-- Link to the page "Reading finished". -->
 </div>
 <div>
-    <a href="pile_to_read.html">Pile to read</a>  <!-- Link to the page "Pile to read". -->
+    <a href="pile_to_read.php">Pile to read</a>  <!-- Link to the page "Pile to read". -->
 </div>
 <div>
-    <a href="wishlist.html">Wishlist</a>  <!-- Link to the page "Wishlist". -->
+    <a href="wishlist.php">Wishlist</a>  <!-- Link to the page "Wishlist". -->
 </div>
 <div>
-    <a href="contact.html">Contact</a>  <!-- Link to the page "Contact". -->
+    <a href="contact.php">Contact</a>  <!-- Link to the page "Contact". -->
 </div>
 <div>
-    <a href="who_am_i.html">Who am I</a>  <!-- Link to the page "Who am I". -->
+    <a href="who_am_i.php">Who am I</a>  <!-- Link to the page "Who am I". -->
 </div>
