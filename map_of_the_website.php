96805985740bc728205cd3c45d0ad21af6797786
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-05-25 10:46:17 +0200

    Create map_of_the_website.php
    File with .php instead of .html

---
+++ map_of_the_website.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">  <!-- The langage of this page is "english" -->
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Map of the website - My life as a reader</title>  <!-- Thumb index of the page. -->
+    <link href="style.css" rel="stylesheet" type="text/css" />  <!-- This file is connect to the file "style.css" for the stylesheet. -->
+  </head>
+  <body>
+    <header>
+     <a href="index.html" class="website_title_index">My life as a reader</a>  <!-- Title of the website and link to index.html -->
+    </header>
+    <a class="page_title">Map of the website</a>  <!-- Title of the page. -->
+    <section class="all_pages_links">
+      <a href="index.html#home">Home</a>
+      <a href="reading_in_progress.html#readinginprogress">Reading in progress</a>
+      <a href="reading_in_progress.html#victor">Victor Hugo - Notre-Dame de Paris</a>
+      <a href="reading_in_progress.html#stephen_king_the_outsider">Stephen King - The outsider</a>
+      <a href="reading_finished.html#reading_finished">Reading finished</a>
+      <a href="reading_finished.html#stephen_king_storm_of_the_century">Stephen King - Storm of the century</a>
+      <a href="reading_finished.html#harry_harrison_make_room_make_room">Harry Harrison - Make room ! Make room !</a>
+      <a href="pile_to_read.html#pile_to_read">Pile to read</a>
+      <a href="pile_to_read.html#walter_tevis_the_queen_s_gambit">Walter Tevis - The Queen's Gambit</a>
+      <a href="pile_to_read.html#stephen_king_night_shift">Stephen King - Night shift</a>
+      <a href="pile_to_read.html#dan_brown_angels_and_demons">Dan Brown - Angels and démons</a>
+      <a href="pile_to_read.html#dan_brown_the_da_vinci_code">Dan Brown - The Da Vinci Code</a>
+      <a href="pile_to_read.html#dan_brown_the_lost_symbol">Dan Brown - The lost symbol</a>
+      <a href="pile_to_read.html#dan_brown_inferno">Dan Brown - Inferno</a>
+      <a href="pile_to_read.html#dan_brown_origin">Dan Brown - Origin</a>
+      <a href="wishlist.html#wishlst">Wishlist</a>
+      <a href="wishlist.html#stephen_king_mr_mercedes">Stephen King - Mr Mercedes</a>
+      <a href="wishlist.html#stephen_king_finders_keepers">Stephen King - Finders Keepers</a>
+      <a href="wishlist.html#stephen_king_end_of_watch">Stephen King - End of watch</a>
+      <a href="contact.html#contact">Contact</a>
+      <a href="who_am_i.html#who_am_i">Who am I</a>
+    </section>
+  </body>
+</html>     
