2e3c475ed2d915cd43e8804a7ab8606b047c5011
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-05-26 18:54:33 +0200

    Update navbar.php
    Line 2, 5, 8, 11, 14, 17 and 20 : new class "navutton" for the special animation for index.html.

--- navbar.php
+++ navbar.php
@@ -1,21 +1,21 @@
 <div>  <!-- Title of the other pages of the website by link. It's in flex-box. -->
-    <a href="index.php">Home</a>  <!-- Link to Home page. -->
+    <a class="navbutton" href="index.php">Home</a>  <!-- Link to Home page. -->
 </div>
 <div>
-    <a href="reading_in_progress.php">Reading in progress</a>  <!-- Link to the page "Reading in progress". -->
+    <a class="navbutton" href="reading_in_progress.php">Reading in progress</a>  <!-- Link to the page "Reading in progress". -->
 </div>
 <div>
-    <a href="reading_finished.php">Reading finished</a>  <!-- Link to the page "Reading finished". -->
+    <a class="navbutton" href="reading_finished.php">Reading finished</a>  <!-- Link to the page "Reading finished". -->
 </div>
 <div>
-    <a href="pile_to_read.php">Pile to read</a>  <!-- Link to the page "Pile to read". -->
+    <a class="navbutton" href="pile_to_read.php">Pile to read</a>  <!-- Link to the page "Pile to read". -->
 </div>
 <div>
-    <a href="wishlist.php">Wishlist</a>  <!-- Link to the page "Wishlist". -->
+    <a class="navbutton" href="wishlist.php">Wishlist</a>  <!-- Link to the page "Wishlist". -->
 </div>
 <div>
-    <a href="contact.php">Contact</a>  <!-- Link to the page "Contact". -->
+    <a class="navbutton" href="contact.php">Contact</a>  <!-- Link to the page "Contact". -->
 </div>
 <div>
-    <a href="who_am_i.php">Who am I</a>  <!-- Link to the page "Who am I". -->
+    <a class="navbutton" href="who_am_i.php">Who am I</a>  <!-- Link to the page "Who am I". -->
 </div>
