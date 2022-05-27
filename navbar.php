bf02314fd28e2356c5334c4c3de9800145fd07ca
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-05-27 23:33:49 +0200

    Update navbar.php
    Line 2, 5, 8, 11, 14, 17 and 20 : class "navbutton" replaced by "navbuttonopacity".

--- navbar.php
+++ navbar.php
@@ -1,21 +1,21 @@
 <div>  <!-- Title of the other pages of the website by link. It's in flex-box. -->
-    <a class="navbutton" href="index.php">Home</a>  <!-- Link to Home page. -->
+    <a class="navbuttonopacity" href="index.php">Home</a>  <!-- Link to Home page. -->
 </div>
 <div>
-    <a class="navbutton" href="reading_in_progress.php">Reading in progress</a>  <!-- Link to the page "Reading in progress". -->
+    <a class="navbuttonopacity" href="reading_in_progress.php">Reading in progress</a>  <!-- Link to the page "Reading in progress". -->
 </div>
 <div>
-    <a class="navbutton" href="reading_finished.php">Reading finished</a>  <!-- Link to the page "Reading finished". -->
+    <a class="navbuttonopacity" href="reading_finished.php">Reading finished</a>  <!-- Link to the page "Reading finished". -->
 </div>
 <div>
-    <a class="navbutton" href="pile_to_read.php">Pile to read</a>  <!-- Link to the page "Pile to read". -->
+    <a class="navbuttonopacity" href="pile_to_read.php">Pile to read</a>  <!-- Link to the page "Pile to read". -->
 </div>
 <div>
-    <a class="navbutton" href="wishlist.php">Wishlist</a>  <!-- Link to the page "Wishlist". -->
+    <a class="navbuttonopacity" href="wishlist.php">Wishlist</a>  <!-- Link to the page "Wishlist". -->
 </div>
 <div>
-    <a class="navbutton" href="contact.php">Contact</a>  <!-- Link to the page "Contact". -->
+    <a class="navbuttonopacity" href="contact.php">Contact</a>  <!-- Link to the page "Contact". -->
 </div>
 <div>
-    <a class="navbutton" href="who_am_i.php">Who am I</a>  <!-- Link to the page "Who am I". -->
+    <a class="navbuttonopacity" href="who_am_i.php">Who am I</a>  <!-- Link to the page "Who am I". -->
 </div>
