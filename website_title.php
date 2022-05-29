cf1443ad3c068dd3a7cfec669a3212f3fb5222dc
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-05-29 17:25:56 +0200

    Update website_title.php
    Line 1 : comment removed.

--- website_title.php
+++ website_title.php
@@ -1,1 +1,1 @@
-<a href="index.php" class="website_title">My life as a reader</a>  <!-- Title of the website and link to index.html -->
+<a href="index.php" class="website_title">My life as a reader</a>
