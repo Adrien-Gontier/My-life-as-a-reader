b785372e84da5310117c046756031d6225c3571f
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-05-31 17:36:58 +0200

    Update navbar.php
    Line 2 : class "navbutton" replaced by "animnavbuttona".
    
    Line 5 : class "navbutton" replaced by "animnavbuttonb".
    
    Line 8 : class "navbutton" replaced by "animnavbuttonc".
    
    Line 11 : class "navbutton" replaced by "animnavbuttond".
    
    Line 14 : class "navbutton" replaced by "animnavbuttone".
    
    Line 17 : class "navbutton" replaced by "animnavbuttonf".
    
    Line 20 : class "navbutton" replaced by "animnavbuttong".

--- navbar.php
+++ navbar.php
@@ -1,21 +1,21 @@
 <div>  <!-- Title of the other pages of the website by link. It's in flex-box. -->
-    <a class="navbutton" href="index.php">Home</a>  <!-- Link to Home page. -->
+    <a class="animnavbuttona" href="index.php">Home</a>  <!-- Link to Home page. -->
 </div>
 <div>
-    <a class="navbutton" href="reading_in_progress.php">Reading in progress</a>  <!-- Link to the page "Reading in progress". -->
+    <a class="animnavbuttonb" href="reading_in_progress.php">Reading in progress</a>  <!-- Link to the page "Reading in progress". -->
 </div>
 <div>
-    <a class="navbutton" href="reading_finished.php">Reading finished</a>  <!-- Link to the page "Reading finished". -->
+    <a class="animnavbuttonc" href="reading_finished.php">Reading finished</a>  <!-- Link to the page "Reading finished". -->
 </div>
 <div>
-    <a class="navbutton" href="pile_to_read.php">Pile to read</a>  <!-- Link to the page "Pile to read". -->
+    <a class="animnavbuttond" href="pile_to_read.php">Pile to read</a>  <!-- Link to the page "Pile to read". -->
 </div>
 <div>
-    <a class="navbutton" href="wishlist.php">Wishlist</a>  <!-- Link to the page "Wishlist". -->
+    <a class="animnavbuttone" href="wishlist.php">Wishlist</a>  <!-- Link to the page "Wishlist". -->
 </div>
 <div>
-    <a class="navbutton" href="contact.php">Contact</a>  <!-- Link to the page "Contact". -->
+    <a class="animnavbuttonf" href="contact.php">Contact</a>  <!-- Link to the page "Contact". -->
 </div>
 <div>
-    <a class="navbutton" href="who_am_i.php">Who am I</a>  <!-- Link to the page "Who am I". -->
+    <a class="animnavbuttong" href="who_am_i.php">Who am I</a>  <!-- Link to the page "Who am I". -->
 </div>
