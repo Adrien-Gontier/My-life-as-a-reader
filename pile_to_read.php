1492640a4df9cd1f984ba59673b3cf610b7a08ef
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-06-03 21:05:34 +0200

    Update pile_to_read.php
    Line 29 : comment which remember me what to do next.

--- pile_to_read.php
+++ pile_to_read.php
@@ -26,6 +26,7 @@
       <a id="pile_to_read" class="page_title">Pile to read</a>  <!-- Title of the page -->
       <img class="picture_15_100_size" src="pictures/pexels-sharon-mccutcheon-1148399.jpg" alt="Books stacked on top of each other">
     </div> 
+    <!-- Put here the links for each book on this page. -->
     <div id="walter_tevis_the_queen_s_gambit" class="books">  <!-- that's where I describe a book I anticipate to read. It's divide in flex-box. -->
       <div>
         <p class="author_name">Walter Tevis</p>  <!-- First name and last name of the author -->
