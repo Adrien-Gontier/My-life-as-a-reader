a04658d3206ff999dee0d0504c0330436f923238
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-06-12 21:04:18 +0200

    Update pile_to_read.php
    Line 35-40 : Dropdown list list of books with links began.

--- pile_to_read.php
+++ pile_to_read.php
@@ -30,7 +30,15 @@
         <img class="picture_15_100_size" src="pictures/pexels-sharon-mccutcheon-1148399.jpg" alt="Books stacked on top of each other">
       </div> 
 
-      <!-- Put here the links for each book on this page, with a dropdown list. -->
+      <!-- Put here the links for each book on this page, with a dropdown list. Will be finish -->
+
+      <select name="Choose books" onChange="location = this.options[this.selectedIndex].value;">
+        <option value="">Select a book</option>
+        <option value="#walter_tevis_the_queen_s_gambit">Walter Tevis - The Queen's Gambit</option>
+        <option value="#stephen_king_night_shift">Stephen King - Night Shift</option>
+        <option value="#dan_brown_angels_and_demons">Dan Brown - Angels and démons</option>
+      </select> 
+
 
       <!-- Find a animation (possible in JavaScript) for the books get definitive appear when I scroll the page. -->
     
