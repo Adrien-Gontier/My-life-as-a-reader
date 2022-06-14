76dc49aca41087b47efcdd292f83b802511d850d
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-06-14 20:03:17 +0200

    Update reading_in_progress.php
    Line 33 : comment removed because test OK.

--- reading_in_progress.php
+++ reading_in_progress.php
@@ -30,8 +30,6 @@
         <img class="picture_30_100_size" src="pictures/pexels-pixabay-415071.jpg" alt="Book open in half"> 
       </div> 
 
-<!-- Put here the links for each book on this page, with a dropdown list. Will be test -->
-
       <select name="Choose books" onChange="location = this.options[this.selectedIndex].value;">
         <option value="">Select a book</option>
         <option value="#victor_hugo_notre_dame_de_paris">Victor Hugo - Notre-Dame de Paris</option>
