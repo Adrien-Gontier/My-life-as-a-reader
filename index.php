34a9be115c162a102d08bfc0137455ae41ca1c0d
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-06-26 20:47:15 +0200

    Update index.php
    Line 29 : Content removed from line 29 to 33.

--- index.php
+++ index.php
@@ -26,11 +26,7 @@
     <a id="home" class="page_title">Welcome Home</a>  <!-- Title of the page. -->
     <section class="section_welcome_page">
       <article>
-        <p>The last book I read was The Storm of the Century by Stephen King. I really liked the fact that it was written in the form of a screenplay. This gives originality to the work.</p>
-        <p>If not, what's new in my life as a reader? I always have on my bedside table the big cobblestone that is In Search of Lost Time by Marcel Proust. I had started reading it a few years ago, but since then I haven't touched it again. However, he has been watching me for a long time. But I don't think I have much time right now.</p>
-        <p>I don't have a lot of time, because I've been learning computer code since the beginning of the year. this excites me. And at the same time, I really miss reading.</p>
-        <p>I really like spending hours reading on my bed with my cat on my legs, preventing me from moving, like an anvil. I like to see the view of the characters in situations and moments of their lives. To be able to appreciate the unfolding of events. To let myself be transported by the rhythm of their dialogue.</p>
-        <p>In short, I miss reading.</p>
+        <p></p>
       </article>
       <aside>
         <p>My name is Adrien Gontier. I'm a reader. And actually I'm a further IT worker.</p>
