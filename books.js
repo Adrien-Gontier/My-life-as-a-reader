1316e38705f7bd7d3e1ca8a4fb4721a1e393e549
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-06-29 12:08:33 +0200

    Create books.js
    Line 1 : array books created.
    
    Line 3-8  and 11-16 : new book added.

---
+++ books.js
@@ -0,0 +1,20 @@
+const books = [
+    {
+        "author_name": "Victor Hugo",
+        "author_date_born_death": "1802 - 1885",
+        "title_of_the_book": "Notre-Dame de Paris",
+        "book_year_publication": "1832",
+        "book_summary": "The plot takes place in Paris in 1482. The novel is divided into 11 books. régoire is a penniless poet. Quasimodo is tried for an attempted kidnapping. Esmeralda is arrested for a murder. And she's under the influence of witchcraft. The cathedral is obviously a special place in the unfolding of history.The novel is too large to be summarized is a few lines. So much that I can only recommend that you read it.",
+        "status": "RIP"
+    },
+    {
+        "author_name": "Stephen King",
+        "author_date_born_death": "1947 -",
+        "title_of_the_book": "The outsider",
+        "book_year_publication": "2018",
+        "book_summary": "Coming soon . . .",
+        "status": "RIP"
+    },
+    
+
+]
