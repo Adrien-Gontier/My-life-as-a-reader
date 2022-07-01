7dc65fe5c4e2a09dd2fd7c62c447446fd7b96878
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-07-01 21:18:01 +0200

    Update books.js
    Line 2  and 10 : the { removed.
    
    Line 18-25 : new books added.

--- books.js
+++ books.js
@@ -1,5 +1,5 @@
 const books = [
-    {
+    "victor_hugo_notre_dame_de_paris" = {
         "author_name": "Victor Hugo",
         "author_date_born_death": "1802 - 1885",
         "title_of_the_book": "Notre-Dame de Paris",
@@ -7,7 +7,7 @@ const books = [
         "book_summary": "The plot takes place in Paris in 1482. The novel is divided into 11 books. régoire is a penniless poet. Quasimodo is tried for an attempted kidnapping. Esmeralda is arrested for a murder. And she's under the influence of witchcraft. The cathedral is obviously a special place in the unfolding of history.The novel is too large to be summarized is a few lines. So much that I can only recommend that you read it.",
         "status": "RIP"
     },
-    {
+    "stephen_king_the_outsider" = {
         "author_name": "Stephen King",
         "author_date_born_death": "1947 -",
         "title_of_the_book": "The outsider",
@@ -15,6 +15,12 @@ const books = [
         "book_summary": "Coming soon . . .",
         "status": "RIP"
     },
-    
-
+    "stephen_king_storm_of_the_century" = {
+        "author_name": "Stephen King",
+        "author_date_born_death": "1947 -",
+        "title_of_the_book": "Storm of the century",
+        "book_year_publication": "1999",
+        "book_summary": "A powerful storm hits the fictional small island town of Little Tall Island. So powerful that all access to the island is cut off and no one can get out. Martha Clarendon, an elderly resident is brutally killed. Robbie Beals, city manager, investigates. Linoge, who claims to know the repetitive inner workings of Hell, is arrested. Later that evening, Linoge escapes and causes the same dream to all the sleeping townspeople. The next day, Linoge makes three inhabitants disappear. Who are its three inhabitants ?",
+        "status": "RIP"
+    },
 ]
