fa35f0598bf5dbd3ed54a60db24f2ce06b1c524c
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-07-02 20:31:39 +0200

    Update books.js
    Line 24 : status modified.
    
    Line 26-33 : new books added.

--- books.js
+++ books.js
@@ -21,6 +21,14 @@ const books = [
         "title_of_the_book": "Storm of the century",
         "book_year_publication": "1999",
         "book_summary": "A powerful storm hits the fictional small island town of Little Tall Island. So powerful that all access to the island is cut off and no one can get out. Martha Clarendon, an elderly resident is brutally killed. Robbie Beals, city manager, investigates. Linoge, who claims to know the repetitive inner workings of Hell, is arrested. Later that evening, Linoge escapes and causes the same dream to all the sleeping townspeople. The next day, Linoge makes three inhabitants disappear. Who are its three inhabitants ?",
-        "status": "RIP"
+        "status": "RF"
     },
+    "harry_harrison_make_room_make_room" = {
+        "author_name": "Harry Harrison",
+        "author_date_born_death": "1925 - 2012",
+        "title_of_the_book": "Make Room ! Make Room !",
+        "book_year_publication": "1966",
+        "book_summary": "<p>New York 1999, overcrowded city, Andy Rush, then a police inspector, lives in a half-room with Sol, a retired engineer.Following a riot, Billy, who stole some meat, sells some of it to get a job as a messenger.Following his first delivery, he kills a wealthy racketeer, Mike. Andy takes charge of the investigation. During the latter, he falls in love with Shirl and enjoys Mike's luxurious apartment. But the lease coming to an end, they move in together. Sol cannot remain passive in the face of overpopulation and decides to join the march against the cancellation of a bill that aims to control the population. Following an injury, Sol dies. A family will take its place. Billy will eventually die, killed by Andy. The story ends with the screen in Times Square announcing that the United States census is 344 million citizens.",
+        "status": "RF"
+    }
 ]
