19089f1018d71e265b15b7a104fa95a7d85b736e
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-07-06 22:03:24 +0200

    Update books.js
    Line 1 : comment added.
    
    Line 2-13 : book data modified.

--- books.js
+++ books.js
@@ -1,10 +1,16 @@
-const books = [
-    "victor_hugo_notre_dame_de_paris" = {
-        "author_name": "Victor Hugo",
-        "author_date_born_death": "1802 - 1885",
-        "title_of_the_book": "Notre-Dame de Paris",
-        "book_year_publication": "1832",
-        "book_summary": "The plot takes place in Paris in 1482. The novel is divided into 11 books. régoire is a penniless poet. Quasimodo is tried for an attempted kidnapping. Esmeralda is arrested for a murder. And she's under the influence of witchcraft. The cathedral is obviously a special place in the unfolding of history.The novel is too large to be summarized is a few lines. So much that I can only recommend that you read it.",
+const books = [  // Check test with loop if it's work
+    {
+        idBook: "victor_hugo_notre_dame_de_paris",
+        author_name: "Victor Hugo",
+        author_date_born_death: "1802 - 1885",
+        title_of_the_book: "Notre-Dame de Paris",
+        book_year_publication: "1832",
+        book_summary: {
+            P: "The plot takes place in Paris in 1482.",
+            P: "The novel is divided into 11 books.", 
+            P: "Grégoire is a penniless poet. Quasimodo is tried for an attempted kidnapping. Esmeralda is arrested for a murder. And she's under the influence of witchcraft. The cathedral is obviously a special place in the unfolding of history.",
+            P: "The novel is too large to be summarized is a few lines. So much that I can only recommend that you read it."
+        },
         "status": "RIP"
     },
     "stephen_king_the_outsider" = {
