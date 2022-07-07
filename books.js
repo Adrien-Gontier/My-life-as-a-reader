11e4700147bba1b10bc67b273d0f16cdccf8997d
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-07-07 19:29:03 +0200

    Update books.js
    Line 2-77 : hierarchical organization data cleaning will be continue.

--- books.js
+++ books.js
@@ -1,26 +1,30 @@
 const books = [  // Check test with loop if it's work
-    {
-        idBook: "victor_hugo_notre_dame_de_paris",
-        author_name: "Victor Hugo",
-        author_date_born_death: "1802 - 1885",
-        title_of_the_book: "Notre-Dame de Paris",
-        book_year_publication: "1832",
-        book_summary: {
-            P: "The plot takes place in Paris in 1482.",
-            P: "The novel is divided into 11 books.", 
-            P: "Grégoire is a penniless poet. Quasimodo is tried for an attempted kidnapping. Esmeralda is arrested for a murder. And she's under the influence of witchcraft. The cathedral is obviously a special place in the unfolding of history.",
-            P: "The novel is too large to be summarized is a few lines. So much that I can only recommend that you read it."
-        },
-        "status": "RIP"
+{
+    idBook: "victor_hugo_notre_dame_de_paris",
+    author_name: "Victor Hugo",
+    author_date_born_death: "1802 - 1885",
+    title_of_the_book: "Notre-Dame de Paris",
+    book_year_publication: "1832",
+    book_summary: {
+        P: "The plot takes place in Paris in 1482.",
+        P: "The novel is divided into 11 books.", 
+        P: "Grégoire is a penniless poet. Quasimodo is tried for an attempted kidnapping. Esmeralda is arrested for a murder. And she's under the influence of witchcraft. The cathedral is obviously a special place in the unfolding of history.",
+        P: "The novel is too large to be summarized is a few lines. So much that I can only recommend that you read it."
     },
-    "stephen_king_the_outsider" = {
-        "author_name": "Stephen King",
-        "author_date_born_death": "1947 -",
-        "title_of_the_book": "The outsider",
-        "book_year_publication": "2018",
-        "book_summary": "Coming soon . . .",
-        "status": "RIP"
+    "status": "RIP"
+},
+{
+    idBook: "stephen_king_the_outsider",
+    author_name: "Stephen King",
+    author_date_born_death: "1947 -",
+    title_of_the_book: "The outsider",
+    book_year_publication: "2018",
+    book_summary: {
+        P: "Coming soon . . ."
     },
+    "status": "RIP"
+},
+{
     "stephen_king_storm_of_the_century" = {
         "author_name": "Stephen King",
         "author_date_born_death": "1947 -",
@@ -29,6 +33,8 @@ const books = [  // Check test with loop if it's work
         "book_summary": "A powerful storm hits the fictional small island town of Little Tall Island. So powerful that all access to the island is cut off and no one can get out. Martha Clarendon, an elderly resident is brutally killed. Robbie Beals, city manager, investigates. Linoge, who claims to know the repetitive inner workings of Hell, is arrested. Later that evening, Linoge escapes and causes the same dream to all the sleeping townspeople. The next day, Linoge makes three inhabitants disappear. Who are its three inhabitants ?",
         "status": "RF"
     },
+},
+{
     "harry_harrison_make_room_make_room" = {
         "author_name": "Harry Harrison",
         "author_date_born_death": "1925 - 2012",
@@ -37,6 +43,8 @@ const books = [  // Check test with loop if it's work
         "book_summary": "New York 1999, overcrowded city, Andy Rush, then a police inspector, lives in a half-room with Sol, a retired engineer.Following a riot, Billy, who stole some meat, sells some of it to get a job as a messenger.Following his first delivery, he kills a wealthy racketeer, Mike. Andy takes charge of the investigation. During the latter, he falls in love with Shirl and enjoys Mike's luxurious apartment. But the lease coming to an end, they move in together. Sol cannot remain passive in the face of overpopulation and decides to join the march against the cancellation of a bill that aims to control the population. Following an injury, Sol dies. A family will take its place. Billy will eventually die, killed by Andy. The story ends with the screen in Times Square announcing that the United States census is 344 million citizens.",
         "status": "RF"
     },
+},
+{
     "walter_tevis_the_queen_s_gambit" = {
         "author_name": "Walter Tevis",
         "author_date_born_death": "1928 - 1984",
@@ -45,6 +53,8 @@ const books = [  // Check test with loop if it's work
         "book_summary": "During her childhood, Beth learns the failures of Mister Shaibel. A few months later, she is introduced to a high school teacher who runs a chess club. At the age of 13, Beth was adopted by a couple who separated shortly afterwards. At a local high school chess tournament, she wins. From tournament to tournament, she is noticed as a prodigy. Finally, following an international career, she meets a group of men in a park who play for love of the game.",
         "status": "PTR"
     },
+},
+{
     "stephen_king_night_shift" = {
         "author_name": "Stephen King",
         "author_date_born_death": "1947 - ",
@@ -53,6 +63,8 @@ const books = [  // Check test with loop if it's work
         "book_summary": "<p>A museum guide discovers a strange curse weighing on his family. A police inspector investigates a series of deaths in a laundry. A contract killer receives a package with toy soldiers who start attacking him. A man approaches a company to mow his lawn. A young man euthanizes his mother who has cancer. Such are, among other things, the adventures of collecting short stories.",
         "status": "PTR"
     },
+},
+{
     "dan_brown_angels_and_demons" = {
         "author_name": "Dan Brown",
         "author_date_born_death": "1964 -", 
@@ -61,4 +73,5 @@ const books = [  // Check test with loop if it's work
         "book_summary": "A prominent person in the CERN lab is murdered and one of the capsules containing antimatter is stolen. The pope dies. A video, showing the four favorites to his estate, is sent to the attention of the Vatican by a killer. The director of CERN calls on Robert Langdon, a professor at Harvard University. Favorites are run one after another through Rome. Discover Robert Langton in this first adventure. The famous specialist of the religious symbology.",
         "status": "PTR"
     }
+}
 ]
