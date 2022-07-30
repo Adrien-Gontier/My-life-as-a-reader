85f5364d9124693b52f69b5f9c661a239a245069
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-07-30 21:09:37 +0200

    Update pile_to_read.js
    Line 5 : constant created for where the DOM will be manipulated.
    
    Line 7-22 : loop has started to be created with conditional.

--- pile_to_read.js
+++ pile_to_read.js
@@ -2,4 +2,32 @@
 
 console.log(books);
 
+const booksList = document.getElementById('books-list');
 
+for (let i = 0 ; 0 < books.length ; i++) {
+    
+    if (books[i].category === 'PTR') {
+        
+        let div = document.createElement('div');
+        div.id = `${books[i].idBook}`;
+        booksList.appendChild(div);
+        
+        // will be continuing
+        
+        
+        
+        
+    }
+    
+}
+
+
+
+
+
+// let newParagraph = document.createElement('p');
+//   newParagraph.innerText = `${episode.title}
+// ${episode.duration} minutes
+// ${episode.hasBeenWatched ? 'Already been watched' : 'Not yet watched'}`;
+//   newDiv.append(newTitle);
+//   newDiv.append(newParagraph);
