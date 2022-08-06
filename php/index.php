b84f46440408a6ec13062474eec452f3b6de2876
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-08-06 09:57:10 +0200

    Create index.php
    Files and directories organization updated.

---
+++ php/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">  <!-- The langage of this page is "english" -->
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Welcome - My life as a reader</title>  <!-- Thumb index of the page. -->
+    <link href="style_all_website.css" rel="stylesheet" type="text/css" />
+    <link href="style_index_website_title.css" rel="stylesheet" type="text/css"/>
+    <link href="style_navbar.css" rel="stylesheet" type="text/css" />
+    <link href="style_index_navbar.css" rel="stylesheet" type="text/css" />
+    <link href="style_page_title_and_picture.css" rel="stylesheet" type="text/css" />
+    <link href="style_index_body.css" rel="stylesheet" type="text/css" />
+    <link href="style_footer.css" rel="stylesheet" type="text/css"/>
+  </head>
+  <body>
+    <header>
+
+      <?php include('website_title.php'); ?>
+
+      <nav>
+
+        <?php include('navbar.php'); ?>
+        
+      </nav>
+    </header>
+    <a id="home" class="page_title">Welcome Home</a>  <!-- Title of the page. -->
+    <section class="section_welcome_page">
+      <article>
+        <p></p>
+      </article>
+      <aside>
+        <p>My name is Adrien Gontier. I'm a reader. And actually I'm a further IT worker.</p>
+      </aside>
+    </section>
+    <footer>
+
+      <?php include('footer.php'); ?>
+    
+    </footer>
+  </body>
+</html>
