9138912ebaa9b2ee03351d84fb4b60ecbb821ca6
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-08-06 09:57:13 +0200

    Create legals_mentions.php
    Files and directories organization updated.

---
+++ php/legals_mentions.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">  <!-- The langage of this page is "english" -->
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Legals mentions - My life as a reader</title>  <!-- Thumb index of the page. -->
+    <link href="style_all_website.css" rel="stylesheet" type="text/css" />
+    <link href="style_website_title.css" rel="stylesheet" type="text/css" />
+    <link href="style_navbar.css" rel="stylesheet" type="text/css" />
+    <link href="style_page_title_and_picture.css" rel="stylesheet" type="text/css" />
+    <link href="style_footer.css" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <header>
+
+      <?php include('website_title.php'); ?>
+
+    </header>
+    
+    <a class="page_title">Legals mentions</a>  <!-- Title of the page. -->
+
+  </body>
+</html>      
