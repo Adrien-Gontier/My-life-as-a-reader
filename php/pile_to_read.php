0520e85161bc5add737b8c3c7ad8a9d92a0e06ef
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-08-07 12:06:47 +0200

    Update pile_to_read.php
    Line 7, 8, 9, 10, 11, 12, 13, 31, 213 and 214 : files path fixed.

--- php/pile_to_read.php
+++ php/pile_to_read.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Pile to read - My life as a reader</title>  <!-- Thumb index of the page. -->
-    <link href="style_all_website.css" rel="stylesheet" type="text/css" />
-    <link href="style_website_title.css" rel="stylesheet" type="text/css" />
-    <link href="style_navbar.css" rel="stylesheet" type="text/css" />
-    <link href="style_but_index_navbar.css" rel="stylesheet" type="text/css" />
-    <link href="style_page_title_and_picture.css" rel="stylesheet" type="text/css" />
-    <link href="style_books_content.css" rel="stylesheet" type="text/css" />
-    <link href="style_footer.css" rel="stylesheet" type="text/css" />
+    <link href="../css/style_all_website.css" rel="stylesheet" type="text/css" />
+    <link href="../css/style_website_title.css" rel="stylesheet" type="text/css" />
+    <link href="../css/style_navbar.css" rel="stylesheet" type="text/css" />
+    <link href="../css/style_but_index_navbar.css" rel="stylesheet" type="text/css" />
+    <link href="../css/style_page_title_and_picture.css" rel="stylesheet" type="text/css" />
+    <link href="../css/style_books_content.css" rel="stylesheet" type="text/css" />
+    <link href="../css/style_footer.css" rel="stylesheet" type="text/css" />
   </head>
   <body>
     <header>
@@ -28,7 +28,7 @@
 
       <div class="page_title_head">
         <a id="pile_to_read" class="page_title">Pile to read</a>  <!-- Title of the page -->
-        <img class="picture_15_100_size" src="pictures/pexels-sharon-mccutcheon-1148399.jpg" alt="Books stacked on top of each other">
+        <img class="picture_15_100_size" src="../pictures/pictures/pexels-sharon-mccutcheon-1148399.jpg" alt="Books stacked on top of each other">
       </div> 
 
       
@@ -210,8 +210,8 @@
     
     </footer>
 
-    <script src="books.js"></script>
-    <script src="pile_to_read.js"></script>
+    <script src="../js/books.js"></script>
+    <script src="../js/pile_to_read.js"></script>
 
   </body>
 </html>
