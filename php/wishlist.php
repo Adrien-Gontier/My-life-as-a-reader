301516285152ce77cb9970337b8617d196794659
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-08-07 12:09:05 +0200

    Update wishlist.php
    Line 7, 8, 9, 10, 11, 12, 13 and 31 : files path fixed.

--- php/wishlist.php
+++ php/wishlist.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Wishlist - My life as a reader</title>  <!-- Thumb index of the page. -->
-    <link href="style_all_website.css" rel="stylesheet" type="text/css" />
-    <link href="style_website_title.css" rel="stylesheet" type="text/css" />
-    <link href="style_navbar.css" rel="stylesheet" type="text/css" />
-    <link href="style_but_index_navbar.css" rel="stylesheet" type="text/css" />
-    <link href="style_page_title_and_picture.css" rel="stylesheet" type="text/css" />
-    <link href="style_books_content.css" rel="stylesheet" type="text/css" />
-    <link href="style_footer.css" rel="stylesheet" type="text/css" />
+    <link href="../css/style_all_website.css" rel="stylesheet" type="text/css" />
+    <link href="../css/style_website_title.css" rel="stylesheet" type="text/css" />
+    <link href="../css/style_navbar.css" rel="stylesheet" type="text/css" />
+    <link href="../css/style_but_index_navbar.css" rel="stylesheet" type="text/css" />
+    <link href="../css/style_page_title_and_picture.css" rel="stylesheet" type="text/css" />
+    <link href="../css/style_books_content.css" rel="stylesheet" type="text/css" />
+    <link href="../css/style_footer.css" rel="stylesheet" type="text/css" />
   </head>
   <body>
     <header>
@@ -28,7 +28,7 @@
 
       <div class="page_title_head">
         <a id="wishlist" class="page_title">Wishlist</a>  <!-- Title of the page. -->
-        <img class="picture_30_100_size" src="pictures/pexels-abby-chung-1106468.jpg" alt="Women in front of shelves full of books"> 
+        <img class="picture_30_100_size" src="../pictures/pexels-abby-chung-1106468.jpg" alt="Women in front of shelves full of books"> 
       </div> 
 
 
