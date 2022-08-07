bedd53caba21db76b0b1771fcc160d98029c1de9
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-08-07 12:07:15 +0200

    Update reading_in_progress.php
    Line 7, 8, 9, 10, 11, 12, 13 and 31 : files path fixed.

--- php/reading_in_progress.php
+++ php/reading_in_progress.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Reading in progress - My life as a reader</title>  <!-- Thumb index of the page. -->
-    <link href="style_all_website.css" rel="stylesheet" type="text/css" />
-    <link href="style_website_title.css" rel="stylesheet" type="text/css" />
-    <link href="style_navbar.css" rel="stylesheet" type="text/css" />
-    <link href="style_but_index_navbar.css" rel="stylesheet" type="text/css" />
-    <link href="style_page_title_and_picture.css" rel="stylesheet" type="text/css" />
-    <link href="style_books_content.css" rel="stylesheet" type="text/css" />
-    <link href="style_footer.css" rel="stylesheet" type="text/css" />
+    <link href="../css/style_all_website.css" rel="stylesheet" type="text/css" />
+    <link href="../css/style_website_title.css" rel="stylesheet" type="text/css" />
+    <link href="../css/style_navbar.css" rel="stylesheet" type="text/css" />
+    <link href="../css/style_but_index_navbar.css" rel="stylesheet" type="text/css" />
+    <link href="../css/style_page_title_and_picture.css" rel="stylesheet" type="text/css" />
+    <link href="../css/style_books_content.css" rel="stylesheet" type="text/css" />
+    <link href="../css/style_footer.css" rel="stylesheet" type="text/css" />
   </head>
   <body>
     <header>
@@ -28,7 +28,7 @@
 
       <div class="page_title_head">
         <a id="reading_in_progress" class="page_title">Reading in progress</a>  <!-- Title of the page. -->
-        <img class="picture_30_100_size" src="pictures/pexels-pixabay-415071.jpg" alt="Book open in half"> 
+        <img class="picture_30_100_size" src="../pictures/pictures/pexels-pixabay-415071.jpg" alt="Book open in half"> 
       </div> 
 
 
