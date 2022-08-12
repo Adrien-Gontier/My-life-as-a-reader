48dc8104ac04970a44e696bcc26878d392bd660d
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-08-12 21:21:50 +0200

    Update reading_in_progress.php
    Line 46-80 : book content removed.
    
    Line 47 : section tag with attribut id book-list added.
    
    Line 58-60 : script tag with book source added.

--- php/reading_in_progress.php
+++ php/reading_in_progress.php
@@ -43,41 +43,9 @@
       
       <!-- Find a animation (possible in JavaScript) for the books get definitive appear when I scroll the page. -->
     
-      <div id="victor_hugo_notre_dame_de_paris" class="books">  <!-- that's where I describe a book I anticipate to read. It's divide in flex-box. -->
-        <div>
-          <p class="author_name">Victor Hugo</p>  <!-- First name and last name of the author -->
-          <p class="author_date_born_death">1802 - 1885</p>  <!-- Year of born and year of death of the author. -->
-          <p class="title_of_the_book">Notre-Dame de Paris</p>  <!-- Title of the book. -->
-          <p class="book_year_publication">1832</p>  <!-- Year of the publication of the book. If it's more than one year : choose the most relevant year. -->
-        </div>
-        <div>  
-          <p class="summary_title">Summary</p>  <!-- Title : "summary" of the book. -->
-          <section class="book_summary">  <!-- Summary of the book. From wikipedia, mostly. -->
-            <p>The plot takes place in Paris in 1482.</p>
-            <p>The novel is divided into 11 books.</p>
-            <p>Grégoire is a penniless poet. Quasimodo is tried for an attempted kidnapping.
-               Esmeralda is arrested for a murder. And she's under the influence of witchcraft.
-               The cathedral is obviously a special place in the unfolding of history.</p>
-            <p>The novel is too large to be summarized is a few lines. So much that I can only recommend that you read it.</p>
-          </section>
-        </div>
-      </div>
+      
+      <section id="books-list"></section>
 
-      <hr class="separation_line_between_books">
-
-      <div id="stephen_king_the_outsider" class="books">  <!-- that's where I describe a book I anticipate to read. It's divide in flex-box. -->
-        <div>
-          <p class="author_name">Stephen King</p>  <!-- First name and last name of the author -->
-          <p class="author_date_born_death">1947 -</p>  <!-- Year of born and year of death of the author. -->
-          <p class="title_of_the_book">The outsider</p>  <!-- Title of the book. -->
-          <p class="book_year_publication">2018</p>  <!-- Year of the publication of the book. If it's more than one year : choose the most relevant year. -->
-        </div>
-        <div>  
-          <p class="summary_title">Summary</p>  <!-- Title : "summary" of the book. -->
-          <section class="book_summary"><p>Coming soon ...</p>
-          </section>  <!-- Summary of the book. From wikipedia, mostly. -->
-        </div>
-      </div>
 
     </main>
 
@@ -86,5 +54,10 @@
       <?php include('footer.php'); ?>
     
     </footer>
+
+    <script src="../js/books.js"></script>
+    <script src="../js/book_loop.js"></script>
+    <script src="../js/reading_in_progress.js"></script>
+
   </body>
 </html>
