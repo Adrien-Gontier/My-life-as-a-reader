0bda298eaeb3da64ffbeb7ca33afbc5d1438321e
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-08-19 11:16:42 +0200

    Update map_of_the_website.js
    Line 2 : remove by class name to by id.
    
    Line 6-9 : first element created.
    
    Line 14 : comment for what to do next added.

--- js/map_of_the_website.js
+++ js/map_of_the_website.js
@@ -1,8 +1,12 @@
 
-const websiteMap = document.getElementsByClassName('all_pages_links');
+const websiteMap = document.getElementById('all_pages_links');
 
 function afficherMapOfTheWebsite() {
 
+    let home = document.createElement('a');
+    home.href = 'index.php#home';
+    home.innerText = 'Home';
+    websiteMap.appendChild(home);
 
 
 }
@@ -11,7 +15,7 @@ function afficherMapOfTheWebsite() {
 
 // Will concatenate the author's name and the book's name for what to display.
 
-
+// Will be continu
 
 
 
