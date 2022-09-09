4522e3bc39e7a5eb1a185790b8a22b894c6396c3
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-09-09 18:43:52 +0200

    Update navbar.php
    Line 5 and 11-14 : links order to other pages changed.

--- php/navbar.php
+++ php/navbar.php
@@ -1,17 +1,17 @@
 <div>  <!-- Title of the other pages of the website by link. It's in flex-box. -->
     <a class="animnavbuttona" href="index.php">Home</a>  <!-- Link to Home page. -->
 </div>
-<div> <!-- Order of the links will be change -->
-    <a class="animnavbuttonb" href="reading_in_progress.php">Reading in progress</a>  <!-- Link to the page "Reading in progress". -->
-</div>
 <div>
-    <a class="animnavbuttonc" href="reading_finished.php">Reading finished</a>  <!-- Link to the page "Reading finished". -->
+    <a class="animnavbuttone" href="wishlist.php">Wishlist</a>  <!-- Link to the page "Wishlist". -->
 </div>
 <div>
     <a class="animnavbuttond" href="pile_to_read.php">Pile to read</a>  <!-- Link to the page "Pile to read". -->
 </div>
 <div>
-    <a class="animnavbuttone" href="wishlist.php">Wishlist</a>  <!-- Link to the page "Wishlist". -->
+    <a class="animnavbuttonb" href="reading_in_progress.php">Reading in progress</a>  <!-- Link to the page "Reading in progress". -->
+</div>
+<div>
+    <a class="animnavbuttonc" href="reading_finished.php">Reading finished</a>  <!-- Link to the page "Reading finished". -->
 </div>
 <div>
     <a class="animnavbuttonf" href="contact.php">Contact</a>  <!-- Link to the page "Contact". -->
