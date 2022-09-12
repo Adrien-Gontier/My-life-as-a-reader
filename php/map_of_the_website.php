b4a16d0ea51c25f9a4724a3ad946d24ad435d10c
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-09-12 10:22:05 +0200

    Update map_of_the_website.php
    Line 33-55 : all section tag and content inside removed.

--- php/map_of_the_website.php
+++ php/map_of_the_website.php
@@ -30,30 +30,6 @@
     
     <section id="all_pages_links"></section>
 
-    <section class="all_pages_links"> <!-- WILL BE REMOVED WHEN THE MANIPULATION OF DOM IS READY -->
-      <a href="index.php#home">Home</a> <!-- Manipulate with DOM js -->
-      <a href="reading_in_progress.php#readinginprogress">Reading in progress</a>
-      <a href="reading_in_progress.php#victor_hugo_notre_dame_de_paris">Victor Hugo - Notre-Dame de Paris</a>
-      <a href="reading_in_progress.php#stephen_king_the_outsider">Stephen King - The outsider</a>
-      <a href="reading_finished.php#reading_finished">Reading finished</a>
-      <a href="reading_finished.php#stephen_king_storm_of_the_century">Stephen King - Storm of the century</a>
-      <a href="reading_finished.php#harry_harrison_make_room_make_room">Harry Harrison - Make room ! Make room !</a>
-      <a href="pile_to_read.php#pile_to_read">Pile to read</a>
-      <a href="pile_to_read.php#walter_tevis_the_queen_s_gambit">Walter Tevis - The Queen's Gambit</a>
-      <a href="pile_to_read.php#stephen_king_night_shift">Stephen King - Night shift</a>
-      <a href="pile_to_read.php#dan_brown_angels_and_demons">Dan Brown - Angels and démons</a>
-      <a href="pile_to_read.php#dan_brown_the_da_vinci_code">Dan Brown - The Da Vinci Code</a>
-      <a href="pile_to_read.php#dan_brown_the_lost_symbol">Dan Brown - The lost symbol</a>
-      <a href="pile_to_read.php#dan_brown_inferno">Dan Brown - Inferno</a>
-      <a href="pile_to_read.php#dan_brown_origin">Dan Brown - Origin</a>
-      <a href="wishlist.php#wishlst">Wishlist</a>
-      <a href="wishlist.php#stephen_king_mr_mercedes">Stephen King - Mr Mercedes</a>
-      <a href="wishlist.php#stephen_king_finders_keepers">Stephen King - Finders Keepers</a>
-      <a href="wishlist.php#stephen_king_end_of_watch">Stephen King - End of watch</a>
-      <a href="contact.php#contact">Contact</a>
-      <a href="who_am_i.php#who_am_i">Who am I</a>
-    </section>
-
     </main>
 
     <footer>
