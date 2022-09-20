3d5a91c5b312efb466b9763feec117fe4cdcff84
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-09-20 13:05:06 +0200

    Create Navbar.jsx
    New component.
    
    Line 1-25 : begun to write.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+export default function Navbar() { // TODO : will be continue
+    <div>
+        <div> 
+            <p class="animnavbuttona" href="index.php">Home</p> 
+        </div>
+        <div>
+            <p class="animnavbuttone" href="wishlist.php">Wishlist</p> 
+        </div>
+        <div>
+            <p class="animnavbuttond" href="pile_to_read.php">Pile to read</p> 
+        </div>
+        <div>
+            <p class="animnavbuttonb" href="reading_in_progress.php">Reading in progress</p> 
+        </div>
+        <div>
+            <p class="animnavbuttonc" href="reading_finished.php">Reading finished</p>  
+        </div>
+        <div>
+            <p class="animnavbuttonf" href="contact.php">Contact</p> 
+        </div>
+        <div>
+            <p class="animnavbuttong" href="who_am_i.php">Who am I</p>
+        </div>
+    </div>
+}
