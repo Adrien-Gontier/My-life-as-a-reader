41d3f18b56774e2fb6e5adcef9ff2248c0d0e373
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-09-22 09:50:29 +0200

    Update Navbar.jsx
    Line 4, 7, 10, 13, 16, 19 and 22 : class and href removed.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,25 @@
 export default function Navbar() { // TODO : will be continue
     <div>
         <div> 
-            <p class="animnavbuttona" href="index.php">Home</p> 
+            <p>Home</p> 
         </div>
         <div>
-            <p class="animnavbuttone" href="wishlist.php">Wishlist</p> 
+            <p>Wishlist</p> 
         </div>
         <div>
-            <p class="animnavbuttond" href="pile_to_read.php">Pile to read</p> 
+            <p>Pile to read</p> 
         </div>
         <div>
-            <p class="animnavbuttonb" href="reading_in_progress.php">Reading in progress</p> 
+            <p>Reading in progress</p> 
         </div>
         <div>
-            <p class="animnavbuttonc" href="reading_finished.php">Reading finished</p>  
+            <p>Reading finished</p>  
         </div>
         <div>
-            <p class="animnavbuttonf" href="contact.php">Contact</p> 
+            <p>Contact</p> 
         </div>
         <div>
-            <p class="animnavbuttong" href="who_am_i.php">Who am I</p>
+            <p>Who am I</p>
         </div>
     </div>
 }
