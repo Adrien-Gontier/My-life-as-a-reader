0ffe8fa4a941fedb56f19b3a0db90cff46028541
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-09-25 19:22:30 +0200

    Update App.jsx
    Line 4 : component PileToRead import added.
    Line 5 : component ReadingInProgress import added.
    Line 6 : component ReadingFinished import added.
    
    Line 17 : component Wishlist path added.
    Line 18 : component ReadingInProgress path and routes added.
    Line 19 : component ReadingFinished path and routes added.
    Line 20 : component Wishlist path and routes added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import Title from './components/Title'
 import Wishlist from './components/Wishlist';
 import './App.css'
+import PileToRead from './components/PileToRead';
+import ReadingInProgress from './components/ReadingInProgress';
+import ReadingFinished from './components/ReadingFinished';
 
 export default function App() {
   
@@ -11,8 +14,10 @@ export default function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Navigate to="/home" />} />
-        <Wishlist />
-        
+        <Wishlist path="/wishlist" />
+        <PileToRead path="piletoread" />
+        <ReadingInProgress path="/readinginprogress" />
+        <ReadingFinished path="readingfinished" />
       </Routes>
       <Footer />
     </BrowserRouter>
