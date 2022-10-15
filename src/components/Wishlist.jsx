6348688e6ad3f8d9622f76d1ec85de7bd34e4875
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-10-15 11:31:39 +0200

    Update Wishlist.jsx
    Line 2 : import for the booklist JSON file.
    
    Line 3 : import for the EachBook component.

--- src/components/Wishlist.jsx
+++ src/components/Wishlist.jsx
@@ -1,4 +1,6 @@
 import WomanInFrontOfLibrary from "../assets/pexels-abby-chung-1106468.jpg";
+import Books from "../assets/books.json";
+import EachBook from './EachBook';
 
 export default function Wishlist() {
     <div>
