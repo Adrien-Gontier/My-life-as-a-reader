a4a2700f1dd0b46f4dd4e97440d133da9122d844
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-10-15 11:31:50 +0200

    Update ReadingFinished.jsx
    Line 3 : import for the EachBook component.

--- src/components/ReadingFinished.jsx
+++ src/components/ReadingFinished.jsx
@@ -1,5 +1,6 @@
 import BooksInLibrary from "../assets/pexels-pixabay-159711.jpg";
 import Books from "../assets/books.json";
+import EachBook from './EachBook';
 
 export default function ReadingFinished() {
     <div>
