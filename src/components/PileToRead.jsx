e107f34586bbdabf2307266a87a72530209c2d6f
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-10-15 11:31:47 +0200

    Update PileToRead.jsx
    Line 3 : import for the EachBook component.

--- src/components/PileToRead.jsx
+++ src/components/PileToRead.jsx
@@ -1,5 +1,6 @@
 import PileOfBooks from "../assets/pexels-sharon-mccutcheon-1148399.jpg";
 import Books from "../assets/books.json";
+import EachBook from './EachBook';
 
 
 export default function PileToRead() {
