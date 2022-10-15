72c0d145db04cbdca9265a24b1bee5fa8e6dea10
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-10-15 11:28:36 +0200

    Create Index.jsx
    New component for the homepage.

---
+++ src/components/Index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Index() {
+  return (
+    <div>
+        <a id="home" class="page_title">Welcome Home</a>  {/* Title of the page. */}
+        <section class="section_welcome_page">
+            <article>
+                <p></p>
+            </article>
+            <aside>
+                <p>My name is Adrien Gontier. I'm a reader. And actually I'm a further IT worker.</p>
+            </aside>
+        </section>
+    </div>
+  )
+}
