599f9868106cf1e43bc88b64ae75d94f0f6712f2
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-10-15 11:31:56 +0200

    Update ReadingInProgress.jsx
    Line 3 : import for the EachBook component.

--- src/components/ReadingInProgress.jsx
+++ src/components/ReadingInProgress.jsx
@@ -1,5 +1,6 @@
 import BookOpen from "../assets/pexels-pixabay-415071.jpg"
 import Books from "../assets/books.json";
+import EachBook from './EachBook';
 
 export default function ReadingInProgress() {
     <div>
