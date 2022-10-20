59abd7b2ef8ec83c1e3b5cbf186d559a7188bfd2
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-10-20 20:01:17 +0200

    Update App.jsx
    Line 10 : import from react router added.
    
    Line 11, 12 and 13 : import components added.
    
    Line 24 : Route component Home added.

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,10 @@ import ReadingFinished from './components/ReadingFinished';
 import WhoAmI from './components/WhoAmI';
 import Footer from './components/Footer';
 import MapOfTheWebsite from './components/MapOfTheWebsite';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import React from 'react';
 
 export default function App() {
   
@@ -17,6 +21,7 @@ export default function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/wishlist" element={<Wishlist  />} />
         <Route path="/piletoread" element={<PileToRead  />} />
         <Route path="/readinginprogress" element={<ReadingInProgress />} />
