6cae8fd489af71cbf7f61f816d68b84c4a44546d
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-10-21 21:29:48 +0200

    Update Wishlist.jsx
    New import added and return added.

--- src/components/Wishlist.jsx
+++ src/components/Wishlist.jsx
@@ -1,9 +1,15 @@
+import React from 'react';
+import EachBook from './EachBook';
 import WomanInFrontOfLibrary from "../assets/pexels-abby-chung-1106468.jpg";
 import Books from "../assets/books.json";
-import EachBook from './EachBook';
+
 
 export default function Wishlist() {
-    <div>
-        <p>Hello world Wishlist !</p>
-    </div>
+    return (
+        <div>
+            <p>Hello world Wishlist !</p>
+            <div><EachBook /></div>
+        </div>
+    )
+    
 }
