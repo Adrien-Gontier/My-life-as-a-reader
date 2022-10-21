fac6b44f6d490ebf98d5ee169a52e09982b1d4e8
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-10-21 21:28:46 +0200

    Update ReadingFinished.jsx
    New import added and return added.

--- src/components/ReadingFinished.jsx
+++ src/components/ReadingFinished.jsx
@@ -1,9 +1,14 @@
+import React from 'react';
 import BooksInLibrary from "../assets/pexels-pixabay-159711.jpg";
 import Books from "../assets/books.json";
 import EachBook from './EachBook';
 
 export default function ReadingFinished() {
-    <div>
-        <p>Hello world Reading Finished !</p>
-    </div>
+    return (
+        <div>
+            <p>Hello world Reading Finished !</p>
+            <div><EachBook /></div>
+        </div> 
+    )
+    
 }
