884cc119401fdda6bd17a54daedbb336d35edca0
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-10-21 21:27:38 +0200

    Update Navbar.jsx
    Indentation improved.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,28 +3,28 @@ import React from 'react';
 
 export default function Navbar() { // TODO : will be continue
     return (
-        <div>
-        <div> 
-            <Link to="/home">Home</Link>
-        </div>
-        <div>
-            <Link to="/wishlist">Wishlist</Link>
-        </div>
-        <div>
-            <Link to="/piletoread">Pile to read</Link>
-        </div>
-        <div>
-            <Link to="/piletoread">Reading in progress</Link>
-        </div>
-        <div>
-            <Link to="/readinginprogress">Reading finished</Link> 
-        </div>
-        <div>
-            <Link to="/readingfinished">Contact</Link>
-        </div>
-        <div>
-            <Link to="/whoami">Who am I</Link>
-        </div>
-    </div>
+        <div className="nav">
+            <div> 
+                <Link to="/home">Home</Link>
+            </div>
+            <div>
+                <Link to="/wishlist">Wishlist</Link>
+            </div>
+            <div>
+                <Link to="/piletoread">Pile to read</Link>
+            </div>
+            <div>
+                <Link to="/readinginprogress">Reading in progress</Link>
+            </div>
+            <div>
+                <Link to="/readingfinished">Reading finished</Link> 
+            </div>
+            <div>
+                <Link to="/contact">Contact</Link>
+            </div>
+            <div>
+                <Link to="/whoami">Who am I</Link>
+            </div>
+         </div>
     )
 }
