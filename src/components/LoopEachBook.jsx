4ef70ff7a1cdd065e68cf4196bce5de5477718a8
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-10-23 16:59:35 +0200

    Create LoopEachBook.jsx
    New file component for the loop of each book.

---
+++ src/components/LoopEachBook.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import EachBook from './EachBook';
+
+export default function LoopEachBook() {
+
+    // UseState will be here for the loop
+
+
+  return (
+    <div><EachBook /></div>
+  )
+}
