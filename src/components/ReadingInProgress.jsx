3486d90cb6ff60f7a20a25dd4ab4d74a186536ab
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-10-24 20:32:30 +0200

    Update ReadingInProgress.jsx
    Line 3 : import removed.

--- src/components/ReadingInProgress.jsx
+++ src/components/ReadingInProgress.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import BookOpen from "../assets/pexels-pixabay-415071.jpg"
-import Books from "../assets/books.json";
 import EachBook from './EachBook';
 
 export default function ReadingInProgress() {
