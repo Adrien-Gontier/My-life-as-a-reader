92c943525065d68930105324350972281c9983c1
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-10-24 20:32:15 +0200

    Update ReadingFinished.jsx
    Line 3 : import removed.

--- src/components/ReadingFinished.jsx
+++ src/components/ReadingFinished.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import BooksInLibrary from "../assets/pexels-pixabay-159711.jpg";
-import Books from "../assets/books.json";
 import EachBook from './EachBook';
 
 export default function ReadingFinished() {
