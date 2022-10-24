5300e7cf95f752b1ce889a30db2e64e62d404977
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-10-24 20:32:36 +0200

    Update Wishlist.jsx
    Line 4 : import removed.

--- src/components/Wishlist.jsx
+++ src/components/Wishlist.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import EachBook from './EachBook';
 import WomanInFrontOfLibrary from "../assets/pexels-abby-chung-1106468.jpg";
-import Books from "../assets/books.json";
 
 
 export default function Wishlist() {
