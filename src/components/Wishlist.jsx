a05f17c29946132f27fa38ab42651ea3c6bb58f3
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-10-25 21:38:03 +0200

    Update Wishlist.jsx
    Line 2 : import removed.
    
    Line 10 : component removed.

--- src/components/Wishlist.jsx
+++ src/components/Wishlist.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import EachBook from './EachBook';
+
 import WomanInFrontOfLibrary from "../assets/pexels-abby-chung-1106468.jpg";
 
 
@@ -7,7 +7,7 @@ export default function Wishlist() {
     return (
         <div>
             <p>Hello world Wishlist !</p>
-            <div><EachBook /></div>
+            <div></div>
         </div>
     )
     
