6822ac5c3437eb072cc718ba6a9fb6d05ed2696d
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-10-25 21:36:39 +0200

    Update ReadingFinished.jsx
    Line 3 : import removed.
    
    Line 9 : component removed.

--- src/components/ReadingFinished.jsx
+++ src/components/ReadingFinished.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import BooksInLibrary from "../assets/pexels-pixabay-159711.jpg";
-import EachBook from './EachBook';
+
 
 export default function ReadingFinished() {
     return (
         <div>
             <p>Hello world Reading Finished !</p>
-            <div><EachBook /></div>
+            <div></div>
         </div> 
     )
     
