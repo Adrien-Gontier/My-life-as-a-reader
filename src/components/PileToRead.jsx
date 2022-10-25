e1339bad6bfa5f72e421a00230c277143e470771
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-10-25 21:36:30 +0200

    Update PileToRead.jsx
    Line 2 : import removed.
    
    Line 9 : component removed.

--- src/components/PileToRead.jsx
+++ src/components/PileToRead.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import EachBook from './EachBook';
+
 import PileOfBooks from "../assets/pexels-sharon-mccutcheon-1148399.jpg";
 
 export default function PileToRead() {
     return (
         <div>
             <p >Hello world Pile To Read !</p>
-            <div><EachBook /></div>
+            <div></div>
         </div>
 
     )
