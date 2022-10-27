2a82cb7039b24b8b1c9541c26d85c48fe9ff810d
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-10-27 20:33:18 +0200

    Create Wishlist.jsx
    Files moved to directory : routes.

---
+++ src/routes/Wishlist.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import WomanInFrontOfLibrary from "../assets/pexels-abby-chung-1106468.jpg";
+
+
+export default function Wishlist() {
+    return (
+        <div>
+            <p>Hello world Wishlist !</p>
+            <div></div>
+        </div>
+    )
+    
+}
