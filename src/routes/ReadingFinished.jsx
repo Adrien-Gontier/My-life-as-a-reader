5301795dcd1bd0ff5aba9641b6ab3ea92b787a7a
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-10-27 20:33:09 +0200

    Create ReadingFinished.jsx
    Files moved to directory : routes.

---
+++ src/routes/ReadingFinished.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import BooksInLibrary from "../assets/pexels-pixabay-159711.jpg";
+
+
+export default function ReadingFinished() {
+    return (
+        <div>
+            <p>Hello world Reading Finished !</p>
+            <div></div>
+        </div> 
+    )
+    
+}
