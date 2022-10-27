47f7ee17b6ed1e7ff8f3dcc6b5855745469ec168
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-10-27 20:33:12 +0200

    Create ReadingInProgress.jsx
    Files moved to directory : routes.

---
+++ src/routes/ReadingInProgress.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import BookOpen from "../assets/pexels-pixabay-415071.jpg"
+import LoopEachBook from "./LoopEachBook";
+
+export default function ReadingInProgress() {
+
+    return (
+        <div>
+            <p >Hello world Reading in progress !</p>
+            <div><LoopEachBook /></div>
+        </div>
+
+    )
+}
