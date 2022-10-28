a038eb8d27cde70591305a90cb5aec8929779a89
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-10-28 20:59:35 +0200

    Update MapOfTheWebsite.jsx
    Components RoutesTitles added.

--- src/routes/MapOfTheWebsite.jsx
+++ src/routes/MapOfTheWebsite.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import RoutesTitles from '../components/RoutesTitles'
 
 export default function MapOfTheWebsite() {
   return (
     <div>
         <p>Hello world MapOfTheWebsite !</p>
+        <RoutesTitles />
     </div>
   )
 }
