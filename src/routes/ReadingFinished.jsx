fda86ad296acecf86897125d5f09cb24510c2ad6
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-10-28 20:59:42 +0200

    Update ReadingFinished.jsx
    Components RoutesTitles added.

--- src/routes/ReadingFinished.jsx
+++ src/routes/ReadingFinished.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import BooksInLibrary from "../assets/pexels-pixabay-159711.jpg";
+import RoutesTitles from '../components/RoutesTitles';
 
 
 export default function ReadingFinished() {
     return (
         <div>
             <p>Hello world Reading Finished !</p>
-            <div></div>
+            <RoutesTitles />
         </div> 
     )
     
