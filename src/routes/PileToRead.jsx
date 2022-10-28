07705f70a92fdf2d419e08083dd2b0854a66803a
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-10-28 20:59:39 +0200

    Update PileToRead.jsx
    Components RoutesTitles added.

--- src/routes/PileToRead.jsx
+++ src/routes/PileToRead.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import PileOfBooks from "../assets/pexels-sharon-mccutcheon-1148399.jpg";
+import RoutesTitles from '../components/RoutesTitles';
 
 export default function PileToRead() {
     return (
         <div>
             <p >Hello world Pile To Read !</p>
-            <div></div>
+            <RoutesTitles />
         </div>
 
     )
