4f68cbfeda0b8d340626e95206ceb09d7b5070d4
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-10-28 20:59:52 +0200

    Update Wishlist.jsx
    Components RoutesTitles added.

--- src/routes/Wishlist.jsx
+++ src/routes/Wishlist.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import WomanInFrontOfLibrary from "../assets/pexels-abby-chung-1106468.jpg";
+import RoutesTitles from '../components/RoutesTitles';
 
 
 export default function Wishlist() {
     return (
         <div>
             <p>Hello world Wishlist !</p>
-            <div></div>
+            <RoutesTitles />
         </div>
     )
     
