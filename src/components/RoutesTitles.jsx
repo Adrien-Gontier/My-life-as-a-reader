2fd863e5c3878d0b72603c93893bb06e252d3922
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-10-28 20:57:36 +0200

    Create RoutesTitles.jsx
    New component file for the titles et pictures for each routes.

---
+++ src/components/RoutesTitles.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function RoutesTitles() {
+    return (
+        <div class="page_title_head">
+            <a id="" class="page-title"></a>
+            <img class="" src="" alt="" />
+        </div>
+    )
+}
