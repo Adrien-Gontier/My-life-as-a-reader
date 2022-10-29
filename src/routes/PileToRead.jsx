f3624b5a83838764e35a15e59daa37c2a69d98c2
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-10-29 17:58:37 +0200

    Update PileToRead.jsx
    Props for the picture of the routes added.

--- src/routes/PileToRead.jsx
+++ src/routes/PileToRead.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-
 import PileOfBooks from "../assets/pexels-sharon-mccutcheon-1148399.jpg";
 import RoutesTitles from '../components/RoutesTitles';
 
 export default function PileToRead() {
+
+
+
+
     return (
         <div>
             <p >Hello world Pile To Read !</p>
-            <RoutesTitles />
+            <RoutesTitles props={PileOfBooks} />
         </div>
 
     )
