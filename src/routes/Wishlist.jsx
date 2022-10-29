51ec31350059858f87d6d6f4ec8f11b7c157d306
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-10-29 17:58:46 +0200

    Update Wishlist.jsx
    Props for the picture of the routes added.

--- src/routes/Wishlist.jsx
+++ src/routes/Wishlist.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import WomanInFrontOfLibrary from "../assets/pexels-abby-chung-1106468.jpg";
 import RoutesTitles from '../components/RoutesTitles';
 
@@ -8,7 +7,7 @@ export default function Wishlist() {
     return (
         <div>
             <p>Hello world Wishlist !</p>
-            <RoutesTitles />
+            <RoutesTitles props={WomanInFrontOfLibrary} />
         </div>
     )
     
