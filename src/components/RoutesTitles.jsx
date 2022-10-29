e4620b519145418c062883133adea1479027eacf
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-10-29 17:58:33 +0200

    Update Wishlist.jsx
    Props for the picture of the routes added.
    
    And usePrams for the name of the routes added.

--- src/components/RoutesTitles.jsx
+++ src/components/RoutesTitles.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
-export default function RoutesTitles() {
+export default function RoutesTitles(props) {
+
+    const {pageTitle} = useParams(); 
+    const picture = props.props; // will be checked
+
     return (
         <div class="page_title_head">
-            <a id="" class="page-title"></a>
-            <img class="" src="" alt="" />
+            <a id="" class="page-title">{pageTitle}</a>
+            <img class="" src={picture} alt="" />
         </div>
     )
 }
