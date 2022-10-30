5a3e9162e8c4e4241057f75a44bd35591ce95549
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-10-30 17:16:21 +0100

    Create MapOfTheWebsite.jsx
    The file has been moved.

---
+++ src/components/MapOfTheWebsite.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import RoutesTitles from '../components/RoutesTitles'
+
+export default function MapOfTheWebsite() {
+  return (
+    <div>
+        <p>Hello world MapOfTheWebsite !</p>
+        <RoutesTitles />
+    </div>
+  )
+}
