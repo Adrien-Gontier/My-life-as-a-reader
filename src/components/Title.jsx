3ecc3df81ce8eeb7eb25a201db7be4fe7177bdbb
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-10-30 17:16:37 +0100

    Create Title.jsx
    The file was restore.

---
+++ src/components/Title.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Title() {
+    return (
+        <div>
+            <p className="website-title">My life as a reader</p>
+        </div>
+    )
+    
+}
