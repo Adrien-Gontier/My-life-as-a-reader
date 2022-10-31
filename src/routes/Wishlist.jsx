32ba904334da9976433419844fa0956847246385
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-10-31 17:58:22 +0100

    Update Wishlist.jsx
    Import RoutesTitles component and import associate removed.

--- src/routes/Wishlist.jsx
+++ src/routes/Wishlist.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import WomanInFrontOfLibrary from "../assets/pexels-abby-chung-1106468.jpg";
-import RoutesTitles from '../components/RoutesTitles';
 
 
 export default function Wishlist() {
     return (
         <div>
             <p>Hello world Wishlist !</p>
-            <RoutesTitles props={WomanInFrontOfLibrary} />
+            
         </div>
     )
     
