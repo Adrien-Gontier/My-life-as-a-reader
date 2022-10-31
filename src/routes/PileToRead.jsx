a2cd5d8fb2d31032aa0522d19530b8795efb6647
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-10-31 17:58:09 +0100

    Update PileToRead.jsx
    Import RoutesTitles component and import associate removed.

--- src/routes/PileToRead.jsx
+++ src/routes/PileToRead.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import PileOfBooks from "../assets/pexels-sharon-mccutcheon-1148399.jpg";
-import RoutesTitles from '../components/RoutesTitles';
+
 
 export default function PileToRead() {
 
+    
 
-
+  
 
     return (
         <div>
             <p >Hello world Pile To Read !</p>
-            <RoutesTitles props={PileOfBooks} />
+            
         </div>
 
     )
