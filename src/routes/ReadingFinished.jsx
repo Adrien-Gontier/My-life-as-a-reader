7ec95b6b2d6dd9ad9948e85efcf778c7bbbe5121
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-10-31 17:58:12 +0100

    Update ReadingFinished.jsx
    Import RoutesTitles component and import associate removed.

--- src/routes/ReadingFinished.jsx
+++ src/routes/ReadingFinished.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import BooksInLibrary from "../assets/pexels-pixabay-159711.jpg";
-import RoutesTitles from '../components/RoutesTitles';
 
 
 export default function ReadingFinished() {
     return (
         <div>
             <p>Hello world Reading Finished !</p>
-            <RoutesTitles props={BooksInLibrary} />
+            
         </div> 
     )
     
