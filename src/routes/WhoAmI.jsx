8d4c1f4a70ba88f87e3f9b62696d94cdefcbae72
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-10-31 17:58:18 +0100

    Update WhoAmI.jsx
    Import RoutesTitles component and import associate removed.

--- src/routes/WhoAmI.jsx
+++ src/routes/WhoAmI.jsx
@@ -1,8 +1,8 @@
-import RoutesTitles from "../components/RoutesTitles";
+import React from "react"
 
 export default function WhoAmI() {
     <div>
         <p>Hello world Who Am I</p>
-        <RoutesTitles />
+        
     </div>
 }
