38370f08988732653e9f90ba2b5ec4b92e47f3fe
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-10-31 17:57:58 +0100

    Update MapOfTheWebsite.jsx
    Import RoutesTitles component and import associate removed.

--- src/components/MapOfTheWebsite.jsx
+++ src/components/MapOfTheWebsite.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import RoutesTitles from '../components/RoutesTitles'
+
 
 export default function MapOfTheWebsite() {
   return (
     <div>
         <p>Hello world MapOfTheWebsite !</p>
-        <RoutesTitles />
     </div>
   )
 }
