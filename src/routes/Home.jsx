975eada3b093b24c69d9aee1f1d6d9c140ed92c3
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-10-31 17:58:05 +0100

    Update Home.jsx
    Import RoutesTitles component and import associate removed.

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import RoutesTitles from '../components/RoutesTitles';
+
 
 export default function Home() {
     return (
         <div>
             <p>Home</p>
-            <RoutesTitles />
+            
         </div>
     )
 }
