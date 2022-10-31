7d4b9755732b406d031656dc938f2a1ae6bf6f7b
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-10-31 17:58:15 +0100

    Update ReadingInProgress.jsx
    Import RoutesTitles component and import associate removed.

--- src/routes/ReadingInProgress.jsx
+++ src/routes/ReadingInProgress.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import BookOpen from "../assets/pexels-pixabay-415071.jpg"
 import LoopEachBook from "../components/LoopEachBook";
-import RoutesTitles from "../components/RoutesTitles";
 
 export default function ReadingInProgress() {
 
     return (
         <div>
             <p >Hello world Reading in progress !</p>
-            <RoutesTitles props={BookOpen} />
+            
             <div><LoopEachBook /></div>
         </div>
 
