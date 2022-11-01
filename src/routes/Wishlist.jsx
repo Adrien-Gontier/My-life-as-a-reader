e76c0f491bbb0b8de62db2aea8338fbf83185bda
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-11-01 17:15:16 +0100

    Update Wishlist.jsx
    RoutesTitles component resumed. And improved with array with key value.

--- src/routes/Wishlist.jsx
+++ src/routes/Wishlist.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import WomanInFrontOfLibrary from "../assets/pexels-abby-chung-1106468.jpg";
+import RoutesTitles from '../components/RoutesTitles';
 
 
 export default function Wishlist() {
+
+    const titlePictures = {title: "Reading finished", pictureSrc: WomanInFrontOfLibrary}
+
+
     return (
         <div>
             <p>Hello world Wishlist !</p>
-            
+            <RoutesTitles props={titlePictures} />
         </div>
     )
     
