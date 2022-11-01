4454aebfdb28a93682eef2c8b119bf934dc77652
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-11-01 17:15:13 +0100

    Update ReadingInProgress.jsx
    RoutesTitles component resumed. And improved with array with key value.

--- src/routes/ReadingInProgress.jsx
+++ src/routes/ReadingInProgress.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import BookOpen from "../assets/pexels-pixabay-415071.jpg"
 import LoopEachBook from "../components/LoopEachBook";
+import RoutesTitles from "../components/RoutesTitles";
 
 export default function ReadingInProgress() {
+
+    const titlePictures = {title: "Reading in progress", pictureSrc: BookOpen}
 
     return (
         <div>
             <p >Hello world Reading in progress !</p>
+            <RoutesTitles props={titlePictures} />
             
-            <div><LoopEachBook /></div>
+            
         </div>
 
     )
