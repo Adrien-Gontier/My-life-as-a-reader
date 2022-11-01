b770f538914472d2c32ccfde7312ba9e2582e241
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-11-01 17:15:07 +0100

    Update PileToRead.jsx
    RoutesTitles component resumed. And improved with array with key value.

--- src/routes/PileToRead.jsx
+++ src/routes/PileToRead.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import PileOfBooks from "../assets/pexels-sharon-mccutcheon-1148399.jpg";
+import RoutesTitles from '../components/RoutesTitles';
 
 
 export default function PileToRead() {
 
     
-
+    const titlePictures = {title: "Pile to read", pictureSrc: PileOfBooks}
   
 
     return (
         <div>
-            <p >Hello world Pile To Read !</p>
+            <p>Hello world Pile To Read !</p>
+            <RoutesTitles props={titlePictures} />
             
         </div>
 
