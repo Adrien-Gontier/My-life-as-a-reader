0bf571dcb47db9ea43d044631ef0e8c64b6d0dc5
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-11-01 17:15:10 +0100

    Update ReadingFinished.jsx
    RoutesTitles component resumed. And improved with array with key value.

--- src/routes/ReadingFinished.jsx
+++ src/routes/ReadingFinished.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import BooksInLibrary from "../assets/pexels-pixabay-159711.jpg";
+import RoutesTitles from '../components/RoutesTitles';
 
 
 export default function ReadingFinished() {
+
+
+    const titlePictures = {title: "Reading finished", pictureSrc: BooksInLibrary}
+
+
     return (
         <div>
             <p>Hello world Reading Finished !</p>
-            
+            <RoutesTitles props={titlePictures} />
         </div> 
     )
     
