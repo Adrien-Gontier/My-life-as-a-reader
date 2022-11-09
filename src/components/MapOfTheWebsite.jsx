1dc847cfe8ceb7caebff8fa141f5db206e88875a
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-11-09 17:43:27 +0100

    Update MapOfTheWebsite.jsx
    New className added and Todo added too.

--- src/components/MapOfTheWebsite.jsx
+++ src/components/MapOfTheWebsite.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 
 export default function MapOfTheWebsite() {
   return (
-    <div>
+    <div className="all-pages-links">
         <p>Hello world MapOfTheWebsite !</p>
+        {/* TODO put the links here */}
     </div>
   )
 }
