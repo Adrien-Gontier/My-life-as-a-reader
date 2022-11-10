193aff4a1437e5cb27e45efebfe60d3105304d01
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-11-10 19:27:43 +0100

    Update MapOfTheWebsite.jsx
    Loop and and semi colon for import added.

--- src/components/MapOfTheWebsite.jsx
+++ src/components/MapOfTheWebsite.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import BookList from "../assets/books.json";
 
 
 export default function MapOfTheWebsite() {
+
+
+  const idBooks = BookList.map((eachIdBook => <p key={eachIdBook.id}>{eachIdBook.author_name} {eachIdBook.title_of_the_book}</p> ));
+
+
+  // will be continu
+
   return (
     <div className="all-pages-links">
         <p>Hello world MapOfTheWebsite !</p>
         {/* TODO put the links here */}
+        <div>{idBooks}</div>
     </div>
   )
 }
