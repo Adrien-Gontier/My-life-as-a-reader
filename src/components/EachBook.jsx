64d6458d111a20ca50af06edd9d4f07431e4ff75
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-11-17 18:42:20 +0100

    Update EachBook.jsx
    className begun to improve

--- src/components/EachBook.jsx
+++ src/components/EachBook.jsx
@@ -6,10 +6,10 @@ export default function EachBook(props) {
   const bookData = props.props;
   const inProgressBook = props.propsInProgress;
 
-  const authorName = <p className="author-name">{bookData.author_name}</p>;
-  const authorDateBornDeath = <p className="author-date-born-death">{bookData.author_date_born_death}</p>
-  const titleOfTheBook = <p className="title-of-the-book">{bookData.title_of_the_book}</p>;
-  const bookYearPublication = <p className="book-year-publication">{bookData.book_year_publication}</p>;
+  const authorName = <p className="books__author-name">{bookData.author_name}</p>;
+  const authorDateBornDeath = <p className="books__author-date-born-death">{bookData.author_date_born_death}</p>
+  const titleOfTheBook = <p className="books__title-of-the-book">{bookData.title_of_the_book}</p>;
+  const bookYearPublication = <p className="books__book-year-publication">{bookData.book_year_publication}</p>;
 
   const divTagOne = <div>{authorName}{authorDateBornDeath}{titleOfTheBook}{bookYearPublication}</div>;
 
