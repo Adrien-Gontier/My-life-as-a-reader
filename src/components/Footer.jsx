a644780245fdf1577e147a86975bfd543eb47a2e
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-11-21 20:13:16 +0100

    Update Footer.jsx
    Hello world component removed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,6 @@ import { Link } from 'react-router-dom'
 export default function Footer() {
   return (
     <div className="footer">
-        <p>Hello world Footer !</p>
         <Link to="/mapofthewebsite">Map of the website</Link>
     </div>
   )
