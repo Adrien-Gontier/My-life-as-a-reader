381e1aab98a97bb5b2783cf09f058a81d23b1432
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-11-24 20:15:02 +0100

    Update Home.jsx
    Tags for the home route added.

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -5,9 +5,16 @@ export default function Home() {
     return (
         <div>
             <p>Home</p>
-            
+            <section class="section_welcome_page">
+                <article>
+                    <p>The last book I read is The storm of the century by Stephen king.</p>
+                </article>
+                <aside>
+                    <p>My name is Adrien Gontier. I'm a reader.</p>
+                </aside>
+            </section>
+
         </div>
     )
 }
-    
-    
+
