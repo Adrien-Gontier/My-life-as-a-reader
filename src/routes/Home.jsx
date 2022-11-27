2743d532a34bbbd341961a45e7e097b8e9d6e0e9
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-11-27 16:12:08 +0100

    Update Home.jsx
    Classname for title added. Content begun added.

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -4,13 +4,13 @@ import React from 'react';
 export default function Home() {
     return (
         <div>
-            <p>Home</p>
+            <p className="page-title-head__page-title">Home</p>
             <section class="section_welcome_page">
                 <article>
                     <p>The last book I read is The storm of the century by Stephen king.</p>
                 </article>
                 <aside>
-                    <p>My name is Adrien Gontier. I'm a reader.</p>
+                    <p>My name is Adrien Gontier. I'm a reader. I'm also in a training programm where my purpuse is to learning coding.</p>
                 </aside>
             </section>
 
