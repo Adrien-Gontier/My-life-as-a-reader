880bf75f4e14014784bb7ba3b83b29810a83c920
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-11-28 08:11:25 +0100

    Update Home.jsx
    Content updated.

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -7,7 +7,8 @@ export default function Home() {
             <p className="page-title-head__page-title">Home</p>
             <section class="section_welcome_page">
                 <article>
-                    <p>The last book I read is The storm of the century by Stephen king.</p>
+                    <p>The last book I read is, honestly ? I don't remenber because it's was so far.</p>
+                    <p>I spend all my time learning code.</p>
                 </article>
                 <aside>
                     <p>My name is Adrien Gontier. I'm a reader. I'm also in a training programm where my purpuse is to learning coding.</p>
