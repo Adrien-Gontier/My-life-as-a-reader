d4978c406b201a8a13bc6fbdccb8a83c7a8a5539
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2022-11-30 21:00:16 +0100

    Update Home.jsx
    Content filling continued.

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -9,6 +9,9 @@ export default function Home() {
                 <article>
                     <p>The last book I read is, honestly ? I don't remenber because it's was so far.</p>
                     <p>I spend all my time learning code.</p>
+                    <p>In early 2000's, I read several books from John Grisham.</p>
+                    <p>In the last decade, I read all Harry Potter's books.</p>
+                    <p></p>
                 </article>
                 <aside>
                     <p>My name is Adrien Gontier. I'm a reader. I'm also in a training programm where my purpuse is to learning coding.</p>
