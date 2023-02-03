6bca6085189af6373461ca46d80a30cf5c9b1587
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-02-03 08:10:24 +0100

    Update ReadingInProgress.jsx
    Picture path fixed

--- src/routes/ReadingInProgress.jsx
+++ src/routes/ReadingInProgress.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import BookOpen from "../assets/pexels-pixabay-415071.jpg"
+import BookOpen from "../assets/pictures/pexels-pixabay-415071.jpg"
 import LoopEachBook from "../components/LoopEachBook";
 import RoutesTitles from "../components/RoutesTitles";
 
