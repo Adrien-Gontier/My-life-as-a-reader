29000c37b1206b607b439c413530fac99e0d057e
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-02-22 08:26:18 +0100

    Update App.jsx
    Indentation update

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,31 @@
-import Title from './components/Title'
-import Wishlist from './routes/Wishlist';
-import './App.css'
-import PileToRead from './routes/PileToRead';
-import ReadingInProgress from './routes/ReadingInProgress';
-import ReadingFinished from './routes/ReadingFinished';
-import WhoAmI from './routes/WhoAmI';
-import Footer from './components/Footer';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './routes/Home';
-import React, { useEffect } from 'react';
-import { useState } from 'react';
-import BookDataContext from './context/BookDataContext';
-import MapOfTheWebsite from './routes/MapOfTheWebsite';
-import axios from 'axios';
+import Title from "./components/Title";
+import Wishlist from "./routes/Wishlist";
+import "./App.css";
+import PileToRead from "./routes/PileToRead";
+import ReadingInProgress from "./routes/ReadingInProgress";
+import ReadingFinished from "./routes/ReadingFinished";
+import WhoAmI from "./routes/WhoAmI";
+import Footer from "./components/Footer";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./routes/Home";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import BookDataContext from "./context/BookDataContext";
+import MapOfTheWebsite from "./routes/MapOfTheWebsite";
+import axios from "axios";
 
 export default function App() {
+  const [bookData, setBookData] = useState(null);
 
-  const [bookData, setBookData] = useState(null)
-
-  const API_URL = "https://raw.githubusercontent.com/Adrien-Gontier/My-life-as-a-reader/main/src/assets/books.json";
+  const API_URL =
+    "https://raw.githubusercontent.com/Adrien-Gontier/My-life-as-a-reader/main/src/assets/books.json";
 
   useEffect(() => {
-    axios.get(API_URL)
-      .then(
-        res => {
-          const occurrence = res.data;
-          setBookData(occurrence);
-        })
-
+    axios.get(API_URL).then((res) => {
+      const occurrence = res.data;
+      setBookData(occurrence);
+    });
   }, []);
 
   return (
@@ -49,7 +46,7 @@ export default function App() {
         <Footer />
       </BookDataContext.Provider>
     </BrowserRouter>
-  )
+  );
 }
 
 // TODO : test my code!!
