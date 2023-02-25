85384593e878a7c912c1ff9b87e82b3321c93e54
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-02-25 11:28:51 +0100

    Update Footer.jsx
    Indentation improved

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
     <div className="footer">
-        <Link to="/mapofthewebsite">Map of the website</Link>
+      <Link to="/mapofthewebsite">Map of the website</Link>
     </div>
-  )
+  );
 }
