b61f9b1965142a33d970ba3080cfc2fa218653ec
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-02-28 13:45:01 +0100

    Update Title.jsx
    Indentation improved

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Title() {
-    return (
-        <div>
-            <Link to="/home" className="website-title">My life as a reader</Link>
-        </div>
-    )
-    
+  return (
+    <div>
+      <Link to="/home" className="website-title">
+        My life as a reader
+      </Link>
+    </div>
+  );
 }
