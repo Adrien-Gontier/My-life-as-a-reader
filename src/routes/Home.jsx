6b421204010e9ed4b375ae99a8c974aeff8a2733
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-03-01 09:46:33 +0100

    Update Home.jsx
    Code source format et indentation improved

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,28 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-    return (
-        <div>
-            <h2 className="page-title-head__page-title">Home</h2>
-            <section class="section_welcome_page">
-                <article>
-                    <p>The last book I read is, honestly ? I don't remenber because it's was so far.</p>
-                    <p>I spend all my time learning code.</p>
-                    <p>In early 2000's, I read several books from John Grisham.</p>
-                    <p>In the last decade, I read all Harry Potter's books.</p>
-                    <p>I have a lot of Stephen King's books to read in my personal library.</p>
-                </article>
-                <aside>
-                    <p>My name is Adrien Gontier. I'm a reader. I'm also in a training programm where my purpuse is to learning coding.</p>
-                    <p>I learn coding since the beginning of the year. The stack I learn is HTML, CSS, JavaScript, ReactJS, JAVA, ...</p>
-                    <p>I like what I learn and I enjoy my self. Sometimes it's hard, sometimes not.</p>
-                    <Link to="/whoami">Who am I ?</Link>
-                </aside>
-            </section>
-
-        </div>
-    )
+  return (
+    <div>
+      <h2 className="page-title-head__page-title">Home</h2>
+      <section className="section_welcome_page">
+        <article>
+          <p>
+            The last book I read is, honestly ? I don't remenber because it's
+            was so far.
+          </p>
+          <p>I spend all my time learning code.</p>
+          <p>In early 2000's, I read several books from John Grisham.</p>
+          <p>In the last decade, I read all Harry Potter's books.</p>
+          <p>
+            I have a lot of Stephen King's books to read in my personal library.
+          </p>
+        </article>
+        <aside>
+          <p>
+            My name is Adrien Gontier. I'm a reader. I'm also in a training
+            programm where my purpuse is to learning coding.
+          </p>
+          <p>
+            I learn coding since the beginning of the year. The stack I learn is
+            HTML, CSS, JavaScript, ReactJS, JAVA, ...
+          </p>
+          <p>
+            I like what I learn and I enjoy my self. Sometimes it's hard,
+            sometimes not.
+          </p>
+          <Link to="/whoami">Who am I ?</Link>
+        </aside>
+      </section>
+    </div>
+  );
 }
-
