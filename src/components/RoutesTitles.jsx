91241e156b1bcc390f35f5d9f1552a2f0ba175b9
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-04-09 17:09:47 +0200

    Update RoutesTitles.jsx
    Code format improved.
    
    Code begun to change.

--- src/components/RoutesTitles.jsx
+++ src/components/RoutesTitles.jsx
@@ -1,18 +1,18 @@
-import React from "react";
+import React from 'react'
 
 export default function RoutesTitles({ props }) {
-  const titlePicture = props;
+    const titlePicture = props
 
-  return (
-    <div className="page-title-head">
-      <h1 id="" className="page-title-head__page-title">
-        {titlePicture.title}
-      </h1>
-      <img
-        className="page-title-head__picture"
-        src={titlePicture.pictureSrc}
-        alt=""
-      />
-    </div>
-  );
+    return (
+        <div className="page-title-head">
+            <h1 id="" className="page-title-head__page-title">
+                {titlePicture.title}
+            </h1>
+            <img
+                className="page-title-head__picture"
+                src={titlePicture.pictureSrc}
+                alt=""
+            />
+        </div>
+    )
 }
