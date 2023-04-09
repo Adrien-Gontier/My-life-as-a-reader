30c0b6d64eafa7563034984573d6eac4baab64bb
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-04-09 17:09:13 +0200

    Update Footer.jsx
    Code format improved

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Footer() {
-  return (
-    <div className="footer">
-      <Link to="/mapofthewebsite">Map of the website</Link>
-    </div>
-  );
+    return (
+        <div className="footer">
+            <Link to="/mapofthewebsite">Map of the website</Link>
+        </div>
+    )
 }
