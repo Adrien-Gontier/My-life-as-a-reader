e1e4ee28a84e333213e36ed8f3bfa4009c142d26
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-04-09 17:09:31 +0200

    Update Navbar.jsx
    Code format improved

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,28 @@
-import { Link } from "react-router-dom";
-import React from "react";
+import { Link } from 'react-router-dom'
+import React from 'react'
 
 export default function Navbar() {
-  // TODO : will be continue
-  return (
-    <div className="nav">
-      <div>
-        <Link to="/home">Home</Link>
-      </div>
-      <div>
-        <Link to="/wishlist">Wishlist</Link>
-      </div>
-      <div>
-        <Link to="/piletoread">Pile to read</Link>
-      </div>
-      <div>
-        <Link to="/readinginprogress">Reading in progress</Link>
-      </div>
-      <div>
-        <Link to="/readingfinished">Reading finished</Link>
-      </div>
-      <div>
-        <Link to="/whoami">Who am I</Link>
-      </div>
-    </div>
-  );
+    // TODO : will be continue
+    return (
+        <div className="nav">
+            <div>
+                <Link to="/home">Home</Link>
+            </div>
+            <div>
+                <Link to="/wishlist">Wishlist</Link>
+            </div>
+            <div>
+                <Link to="/piletoread">Pile to read</Link>
+            </div>
+            <div>
+                <Link to="/readinginprogress">Reading in progress</Link>
+            </div>
+            <div>
+                <Link to="/readingfinished">Reading finished</Link>
+            </div>
+            <div>
+                <Link to="/whoami">Who am I</Link>
+            </div>
+        </div>
+    )
 }
