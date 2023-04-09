3e284945e4b19690cc1344595ef99ff933412ce4
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-04-09 17:12:01 +0200

    Update Wishlist.jsx
    Code format improved.

--- src/routes/Wishlist.jsx
+++ src/routes/Wishlist.jsx
@@ -1,18 +1,18 @@
-import React from "react";
-import WomanInFrontOfLibrary from "../assets/pictures/pexels-abby-chung-1106468.jpg";
-import LoopEachBook from "../components/LoopEachBook";
-import RoutesTitles from "../components/RoutesTitles";
+import React from 'react'
+import WomanInFrontOfLibrary from '../assets/pictures/pexels-abby-chung-1106468.jpg'
+import LoopEachBook from '../components/LoopEachBook'
+import RoutesTitles from '../components/RoutesTitles'
 
 export default function Wishlist() {
-  const titlePictures = {
-    title: "Wishlist",
-    pictureSrc: WomanInFrontOfLibrary,
-  };
+    const titlePictures = {
+        title: 'Wishlist',
+        pictureSrc: WomanInFrontOfLibrary,
+    }
 
-  return (
-    <div>
-      <RoutesTitles props={titlePictures} />
-      <LoopEachBook props={"WL"} />
-    </div>
-  );
+    return (
+        <div>
+            <RoutesTitles props={titlePictures} />
+            <LoopEachBook props={'WL'} />
+        </div>
+    )
 }
