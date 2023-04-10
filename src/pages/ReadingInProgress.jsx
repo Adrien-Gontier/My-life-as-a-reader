817868540a9ac1709950a3e44a3da4d8ec3eb498
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-04-10 10:45:57 +0200

    Create ReadingInProgress.jsx
    Directory name routes changed to pages

---
+++ src/pages/ReadingInProgress.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import BookOpen from '../assets/pictures/pexels-pixabay-415071.jpg'
+import LoopEachBook from '../components/LoopEachBook'
+import RoutesTitles from '../components/RoutesTitles'
+
+export default function ReadingInProgress() {
+    const titlePictures = { title: 'Reading in progress', pictureSrc: BookOpen }
+
+    return (
+        <div>
+            <RoutesTitles props={titlePictures} />
+            <LoopEachBook props={'RIP'} />
+        </div>
+    )
+}
