c5e25cb64d782067ba2dd73f428d9788b1c1f4eb
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-04-10 10:45:47 +0200

    Create MapOfTheWebsite.jsx
    Directory name routes changed to pages

---
+++ src/pages/MapOfTheWebsite.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import BookList from '../assets/books.json'
+
+export default function MapOfTheWebsite() {
+    const idBooks = BookList.map((eachIdBook) => (
+        <div key={eachIdBook.id}>
+            <p>{eachIdBook.author_name}</p>
+            <p>{eachIdBook.title_of_the_book}</p>
+        </div>
+    ))
+
+    // will be continu
+
+    return (
+        <div>
+            <p>Map Of The Website</p>
+            {/* TODO put the links here with each routes of the website */}
+            <div className="all-pages-links">{idBooks}</div>
+        </div>
+    )
+}
