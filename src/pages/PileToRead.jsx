16280b0855b217f7fad85b9d782611ba9b8a1b3a
Adrien-Gontier <99264805+Adrien-Gontier@users.noreply.github.com>
2023-04-10 10:45:51 +0200

    Create PileToRead.jsx
    Directory name routes changed to pages

---
+++ src/pages/PileToRead.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PileOfBooks from '../assets/pictures/pexels-sharon-mccutcheon-1148399.jpg'
+import LoopEachBook from '../components/LoopEachBook'
+import RoutesTitles from '../components/RoutesTitles'
+
+export default function PileToRead() {
+    const titlePictures = { title: 'Pile to read', pictureSrc: PileOfBooks }
+
+    return (
+        <div>
+            <RoutesTitles props={titlePictures} />
+            <LoopEachBook props={'PTR'} />
+        </div>
+    )
+}
